74f6f9e7e5a814702327608e114587c3a2d51cdf
antonylima <asc.lima15@gmail.com>
2023-11-22 18:11:27 -0300

    bootstrap

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -5,28 +5,32 @@
 
 
 @section('content')
-
-<h1>ALUNOS</h1>
-<a class="" href="{{route('page-create')}}">NOVO ALUNO</a>
-<table>
+<div class="container text-center">
+  <h1>ALUNOS</h1>
+<a class="btn btn-success m-2" href="{{route('page-create')}}">NOVO</a>
+<table class="table table-dark table-striped ">
   <tr>
     <th>ID</th>
     <th>NOME</th>
-    <th>NASC</th>
+    <th>ANO</th>
+    <th></th>
+    <th></th>
   </tr>
+
 @foreach($resultado as $value)
 <tr>
   <td>{{$value->id}}</td>
   <td>{{$value->nome}}</td>
   <td>{{$value->ano}}</td>
-  <td><a href="{{route('page-edit',[$value->id])}}">E</a></td>
+  <td><a href="{{route('page-edit',[$value->id])}}">EDITAR</a></td>
   <td><form action="{{route('page-destroy',[$value->id])}}" method="POST">
     @csrf
     @method('DELETE')
-    <input type="submit" value="X">
+    <input type="submit" value="DEL">
   </form></td>
 </tr>
 @endforeach
-
 </table>
+
+</div>
 @endsection
